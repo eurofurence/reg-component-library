f834fa97bf94ebb1a1654c2e3e9a12951cdcbcd4
Ruben Tytgat <ruben_tytgat@hotmail.com>
2020-12-06 20:55:02 +0100

    Have 2 font size toggles in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,7 +12,7 @@ export const parameters = {
 }
 
 export const globalTypes = {
-	fontSize: {
+	browserFontSize: {
 		name: 'Font Size',
 		description: 'Browser font size',
 		defaultValue: 16,
@@ -30,15 +30,35 @@ export const globalTypes = {
 				{ value: 24, title: '24px' },
 			]
 		}
-	}
+	},
+	localFontSize: {
+		name: 'Font Size',
+		description: 'Local font size',
+		defaultValue: null,
+		toolbar: {
+			icon: 'zoom',
+			items: [
+				{ value: null, title: '100%' },
+				{ value: 8, title: '8px' },
+				{ value: 10, title: '10px' },
+				{ value: 12, title: '12px' },
+				{ value: 14, title: '14px' },
+				{ value: 16, title: '16px' },
+				{ value: 18, title: '18px' },
+				{ value: 20, title: '20px' },
+				{ value: 22, title: '22px' },
+				{ value: 24, title: '24px' },
+			]
+		}
+	},
 }
 
 export const decorators = [
-	(Story, { globals: { fontSize } }) => {
+	(Story, { globals: { browserFontSize, localFontSize } }) => {
 		useEffect(() => {
-			document.querySelector('html').style.fontSize = `${fontSize * 0.625}px`
+			document.querySelector('html').style.fontSize = `${browserFontSize * 0.625}px`
 		})
 
-		return <Story />
+		return <div style={localFontSize == null ? {} : { fontSize: `${localFontSize}px` }}><Story /></div>
 	}
 ]
