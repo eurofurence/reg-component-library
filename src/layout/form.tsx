2203c83031fdff74c0d112ae4d931265ee67ebe3
Ruben Tytgat <ruben_tytgat@hotmail.com>
2021-11-24 16:29:44 +0100

    Remove margins on forms

--- src/layout/form.tsx
+++ src/layout/form.tsx
@@ -7,8 +7,6 @@ export default styled.form`
 	grid-template-columns: repeat(${COLUMN_COUNT}, 1fr);
 	column-gap: 24px;
 	row-gap: 24px;
-	width: 620px;
-	margin: 0px auto;
 
 	> * {
 		grid-column: span ${COLUMN_COUNT};
