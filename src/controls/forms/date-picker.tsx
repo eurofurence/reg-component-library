b9f42a82cb6ff5a8789ea05f31febb50596390f9
Ruben Tytgat <ruben_tytgat@hotmail.com>
2022-04-05 17:44:49 +0200

    DatePicker initial development

--- src/controls/forms/date-picker.tsx
+++ src/controls/forms/date-picker.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 
-import { ForwardedRef } from 'react'
+import { ForwardedRef, useState } from 'react'
 import styled from '@emotion/styled'
 import { startOfYear, endOfYear } from 'date-fns'
 import FormHeaderLabel from './form-header-label'
 import formControlStyle from './form-control'
 import ReactDatePicker from 'react-datepicker'
+import leftArrow from '../../assets/left-arrow.svg'
+import rightArrow from '../../assets/right-arrow.svg'
 // import 'react-datepicker/dist/react-datepicker.css'
 
 const RangeContainer = styled.section`
@@ -71,15 +73,41 @@ const Overlay = styled.section`
 		border: 2px solid var(--color-grays-300);
 		border-radius: 0.1875em;
 		user-select: none;
+		line-height: 1.171875;
 	}
 
 	.react-datepicker__current-month {
-		// display: none;
+		display: none;
+	}
+
+	.react-datepicker__navigation {
+		position: absolute;
+	}
+
+	.react-datepicker__navigation--previous {
+		top: 36px;
+		left: 24px;
+		z-index: 1;
+	}
+
+	.react-datepicker__navigation--next {
+		top: 36px;
+		right: 24px;
+		z-index: 1;
+	}
+
+	.react-datepicker__navigation-icon--previous {
+		content: url("${leftArrow}")
+	}
+
+	.react-datepicker__navigation-icon--next {
+		content: url("${rightArrow}")
 	}
 
 	.react-datepicker__month-year-dropdown-container {
 		color: var(--color-brand-2-900);
 		position: relative;
+		margin-bottom: 36px;
 	}
 
 	.react-datepicker__month-year-dropdown {
@@ -104,34 +132,86 @@ const Overlay = styled.section`
 	.react-datepicker__day-names, .react-datepicker__week {
 		display: grid;
 		grid-template-columns: repeat(7, 1fr);
-		justify-items: center;
-		align-items: center;
+	}
+
+	.react-datepicker__week {
+		justify-items: stretch;
+		align-items: stretch;
+
+		margin: 13.375px 0px;
 	}
 
 	.react-datepicker__day-names {
+		justify-items: center;
+		align-items: center;
+
 		font-weight: 700;
 		border-bottom: solid 1px var(--color-brand-2-100);
+		padding-bottom: 0.5em;
 	}
 
 	.react-datepicker__month {
 	}
 
-	.react-datepicker__day {
+	.react-datepicker__day-text {
 		width: 2em;
 		height: 2em;
 		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+
+	.react-datepicker__day {
+		display: flex;
 		align-items: center;
 		justify-content: space-around;
+
+		position: relative;
 	}
 
 	.react-datepicker__day--outside-month {
 		color: var(--color-grays-300);
 	}
 
-	.react-datepicker__day--keyboard-selected {
-		background-color: var(--color-semantic-info);
-		color: var(--color-grays-000);
-		border-radius: 1em;
+	.react-datepicker__day--in-range {
+		&::before {
+			content: "";
+
+			background-color: var(--color-semantic-info);
+			opacity: 0.2;
+
+			position: absolute;
+			top: 50%;
+			z-index: -1;
+			transform: translateY(-50%);
+
+			height: 24px;
+		}
+
+		&:not(.react-datepicker__day--range-start):not(.react-datepicker__day--range-end)::before {
+			left: 0px;
+			width: 100%;
+		}
+
+		&.react-datepicker__day--range-start::before {
+			right: 0px;
+			width: 50%;
+		}
+
+		&.react-datepicker__day--range-end::before {
+			left: 0px;
+			width: 50%;
+		}
+	}
+
+	.react-datepicker__day--selected, .react-datepicker__day--range-start, .react-datepicker__day--range-end {
+		.react-datepicker__day-text {
+			width: 2em;
+			height: 2em;
+			background-color: var(--color-semantic-info);
+			border-radius: 1em;
+			color: var(--color-grays-000);
+		}
 	}
 `
 
@@ -145,25 +225,41 @@ const RangeDatePicker = ({
 	endLabel,
 	endValue,
 	endPlaceholder,
-}: RangeDatePickerProps) => <RangeContainer gridSpan={gridSpan}>
-	<Inputs>
-		<FormHeaderLabel label={startLabel}>
-			<Input value={startValue} placeholder={startPlaceholder} ref={startInputRef}/>
-		</FormHeaderLabel>
-		<FormHeaderLabel label={endLabel}>
-			<Input value={endValue} placeholder={endPlaceholder} ref={endInputRef}/>
-		</FormHeaderLabel>
-	</Inputs>
-	<Overlay>
-		<ReactDatePicker
-			inline
-			formatWeekDay={(d: string) => d.slice(0, 1)}
-			minDate={startOfYear(new Date())}
-			maxDate={endOfYear(new Date())}
-			showMonthYearDropdown
-		/>
-	</Overlay>
-</RangeContainer>
+}: RangeDatePickerProps) => {
+	const [startDate, setStartDate] = useState<Date | null>(new Date())
+	const [endDate, setEndDate] = useState<Date | null>(null)
+
+	const onChange = ([start, end]: readonly [Readonly<Date>, Readonly<Date>]) => {
+	  setStartDate(start)
+	  setEndDate(end)
+	}
+
+	return <RangeContainer gridSpan={gridSpan}>
+		<Inputs>
+			<FormHeaderLabel label={startLabel}>
+				<Input value={startValue} placeholder={startPlaceholder} ref={startInputRef}/>
+			</FormHeaderLabel>
+			<FormHeaderLabel label={endLabel}>
+				<Input value={endValue} placeholder={endPlaceholder} ref={endInputRef}/>
+			</FormHeaderLabel>
+		</Inputs>
+		<Overlay>
+			<ReactDatePicker
+				inline
+				formatWeekDay={(d: string) => d.slice(0, 1)}
+				minDate={startOfYear(new Date())}
+				maxDate={endOfYear(new Date())}
+				selectsRange
+				selected={startDate}
+				startDate={startDate}
+				endDate={endDate}
+				onChange={onChange}
+				showMonthYearDropdown
+				renderDayContents={(day: number) => <span className="react-datepicker__day-text">{day}</span>}
+			/>
+		</Overlay>
+	</RangeContainer>
+}
 
 const SimpleDatePicker = ({
 	gridSpan,
