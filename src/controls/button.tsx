f57169da6764a76cda7cd58fc28708947335f5c5
Ruben Tytgat <ruben_tytgat@hotmail.com>
2023-02-03 21:28:44 +0100

    Add inverted button

--- src/controls/button.tsx
+++ src/controls/button.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 
-export default styled.button`
-	background-color: var(--color-semantic-info);
+export default styled.button<{ readonly inverted?: boolean }>`
+	background-color: ${({ inverted = false }) => inverted ? 'var(--color-grays-000)' : 'var(--color-semantic-info)'};
 
 	height: 3em;
 	padding: 0em 2.125em;
@@ -14,7 +14,7 @@ export default styled.button`
 	font-weight: 700;
 	line-height: 1.1875;
 
-	color: var(--color-grays-000);
+	color: ${({ inverted = false }) => inverted ? 'var(--color-brand-2-500)' : 'var(--color-grays-000)'};
 
 	display: flex;
 	align-items: center;
