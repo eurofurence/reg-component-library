8bfa78a9bfe22a032ed085ed6eaadc3f1e555faa
Ruben Tytgat <ruben_tytgat@hotmail.com>
2023-02-10 18:44:47 +0100

    Almost finished navbar

--- src/layout/nav-bar.tsx
+++ src/layout/nav-bar.tsx
@@ -1,21 +1,28 @@
 /* eslint-disable @typescript-eslint/no-use-before-define,max-len */
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
-import { ForwardedRef, forwardRef, MouseEventHandler, ReactNode, useState } from 'react'
+import { ComponentProps, ReactNode, useState } from 'react'
 import { useDetectClickOutside } from 'react-detect-click-outside'
 import { DeepReadonly } from 'ts-essentials'
 import { desktop, mobile } from '../media-queries'
 
 const Hamburger = styled.button`
+	line-height: 0em;
+	height: 64px;
+	width: 64px;
 
+	${desktop} {
+		display: none;
+	}
 `
 
-const NavBarContainer = styled.header`
+export const NavBar = styled.header`
 	background-color: var(--color-brand-2-900);
 	color: var(--color-grays-000);
-	display: flex;
+	display: grid;
 	align-items: center;
-	padding: 2em;
+
+	user-select: none;
 
 	font-family: Roboto;
 	font-weight: 400;
@@ -25,61 +32,106 @@ const NavBarContainer = styled.header`
 	${mobile} {
 		height: 64px;
 		position: relative;
+		grid: "left right" auto
+		      / auto min-content;
 	}
 
 	${desktop} {
 		height: 110px;
+		grid: "left center right" auto
+		      / 1fr 1fr 1fr;
 	}
 `
 
-const MenuContainer = styled.menu<{ readonly isOpen: boolean }>`
-	${mobile} {
-		header > & {
-			position: absolute;
-			left: 0px;
-			top: 100%;
-			background-color: var(--color-brand-2-900);
-			width: 100%;
-		}
+const NavBarSection = styled.section`
+	display: flex;
+	align-items: center;
+	gap: 1ch;
+`
 
-		header > menu & {
-			display: ${({ isOpen }) => isOpen ? 'flex' : 'none'};
-			flex-direction: column;
-			align-items: stretch;
-			margin-left: 1em;
-		}
+export const NavBarTitle = styled(NavBarSection)`
+	grid-area: left;
+	justify-self: start;
+
+	font-family: Manrope;
+	font-weight: 600;
+
+	${mobile} {
+		padding-left: 24px;
+		font-size: 2.4rem;
 	}
 
 	${desktop} {
-		header > & {
-			display: flex;
-			align-items: center;
-			gap: 1em;
-		}
-
-		header > menu & {
-			position: absolute;
-			top: 100%;
-			display: ${({ isOpen }) => isOpen ? 'flex' : 'none'};
-			flex-direction: column;
-			align-items: stretch;
-			background-color: var(--color-brand-2-900);
-			border: solid 2px var(--color-grays-300);
-			border-radius: 3px;
-		}
+		padding-left: 32px;
+		font-size: 3.6rem;
 	}
 `
 
-const MenuItemContainer = styled.li`
+export const NavBarCenter = styled(NavBarSection)`
+	grid-area: center;
+	justify-self: center;
+`
+
+const NavBarRightBase = styled(NavBarSection)`
+	grid-area: right;
+	justify-self: end;
+
+	${desktop} {
+		padding-right: 32px;
+	}
+`
+
+export const NavBarRight = styled(NavBarRightBase)`
+	${mobile} {
+		padding-right: 24px;
+	}
+`
+
+const NavBarMenuContainer = styled.menu<{ readonly isOpen: boolean }>`
+	${mobile} {
+		display: ${({ isOpen }) => isOpen ? 'unset' : 'none'};
+		position: absolute;
+		left: 0px;
+		top: 100%;
+		background-color: var(--color-brand-2-900);
+		width: 100%;
+	}
+
+	${desktop} {
+		display: flex;
+		align-items: center;
+		gap: 1em;
+	}
+`
+
+const NavBarSubMenuContainer = styled.menu<{ readonly isOpen: boolean }>`
+	${mobile} {
+		display: ${({ isOpen }) => isOpen ? 'flex' : 'none'};
+		flex-direction: column;
+		align-items: stretch;
+		margin-left: 1em;
+	}
+
+	${desktop} {
+		position: absolute;
+		top: 100%;
+		right: 0px;
+		display: ${({ isOpen }) => isOpen ? 'flex' : 'none'};
+		flex-direction: column;
+		align-items: stretch;
+		background-color: var(--color-brand-2-900);
+		border: solid 2px var(--color-grays-300);
+		border-radius: 3px;
+	}
+`
+
+const NavBarMenuItemContainer = styled.li`
 	display: block;
 
 	${desktop} {
 		position: relative;
 
-		header > menu > & {
-		}
-
-		header > menu > li > menu & {
+		header > section > menu > li > menu & {
 			:not(:first-child) {
 				border-top: solid 2px var(--color-grays-300);
 			}
@@ -87,12 +139,11 @@ const MenuItemContainer = styled.li`
 	}
 `
 
-const MenuItemLinkContainer = styled.a<{ readonly showArrow: boolean }>`
+const NavBarMenuItemLinkContainer = styled.a<{ readonly showArrow: boolean }>`
 	color: var(--color-grays-000);
 	cursor: pointer;
 	display: flex;
 	align-items: center;
-	padding: 0em 1em;
 	gap: 1em;
 	height: 3em;
 	// line-height: 1.1875em;
@@ -105,71 +156,77 @@ const MenuItemLinkContainer = styled.a<{ readonly showArrow: boolean }>`
 	`}
 
 	${mobile} {
+		padding: 0em 1.5em;
 	}
 
 	${desktop} {
-		header > menu > li > & {
+		padding: 0em 1em;
+
+		header > section > menu > li > & {
 			border: solid 2px var(--color-grays-300);
 			border-radius: 3px;
-		}
-
-		header > menu > li > menu & {
 		}
 	}
 `
 
-interface MenuItemLinkProps {
+type NavBarMenuItemLinkProps = ComponentProps<typeof NavBarMenuItemLinkContainer> & {
 	readonly icon?: string
 	readonly label: string
-	readonly showArrow?: boolean
-	readonly onClick?: MouseEventHandler
-}
-
-const MenuItemLink = ({ icon, label, showArrow = false, onClick }: MenuItemLinkProps) => <MenuItemLinkContainer showArrow={showArrow} onClick={onClick}>
-	{icon === undefined ? undefined : <img src={icon}/>}
-	{label}
-</MenuItemLinkContainer>
-
-const MenuItem = ({ icon, label, subitems = [] }: MenuItemDef) => {
-	if (subitems.length === 0) {
-		return <MenuItemContainer>
-			<MenuItemLink icon={icon} label={label}/>
-		</MenuItemContainer>
-	} else {
-		const [isOpen, setIsOpen] = useState(false)
-
-		const insideRef = useDetectClickOutside({
-			onTriggered: () => setIsOpen(false),
-		})
-
-		return <MenuItemContainer ref={insideRef}>
-			<MenuItemLink icon={icon} label={label} showArrow={true} onClick={() => setIsOpen(!isOpen)}/>
-			<Menu isOpen={isOpen} items={subitems}/>
-		</MenuItemContainer>
-	}
 }
 
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-const Menu = forwardRef(({ isOpen, items }: { readonly isOpen: boolean, readonly items: readonly MenuItemDef[] }, ref: ForwardedRef<HTMLElement>) => <MenuContainer ref={ref} isOpen={isOpen}>
-	{items.map(item => <MenuItem key={item.label} {...item}/>)}
-</MenuContainer>)
+const NavBarMenuItemLink = ({ icon, label, ...rest }: NavBarMenuItemLinkProps) => <NavBarMenuItemLinkContainer {...rest}>
+	{icon === undefined ? undefined : <img src={icon}/>}
+	{label}
+</NavBarMenuItemLinkContainer>
 
-
-export interface MenuItemDef {
-	readonly icon?: string
-	readonly label: string
-	readonly subitems?: readonly MenuItemDef[]
+export type NavBarMenuItemProps = Omit<NavBarMenuItemLinkProps, 'showArrow'> & {
+	readonly children?: DeepReadonly<ReactNode>
 }
 
-export interface NavBarProps {
-	readonly menuItems?: readonly MenuItemDef[]
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+export const NavBarMenuItem = (props: NavBarMenuItemProps) => <NavBarMenuItemContainer>
+	<NavBarMenuItemLink showArrow={false} {...props} />
+</NavBarMenuItemContainer>
+
+export interface NavBarSubMenuProps {
+	readonly icon?: string
+	readonly label: string
 	readonly children: DeepReadonly<ReactNode>
 }
 
-const NavBar = ({ menuItems = [], children }: NavBarProps) => <NavBarContainer>
-	{children}
-	<Hamburger><img src="data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAyMCAxNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNMjAgMlYwSDBWMkgyMFpNMjAgNlY4SDBWNkgyMFpNMjAgMTJWMTRIMFYxMkgyMFoiIGZpbGw9IndoaXRlIi8+DQo8L3N2Zz4="/></Hamburger>
-	{menuItems.length === 0 ? undefined : <Menu isOpen={true} items={menuItems}/>}
-</NavBarContainer>
+export const NavBarSubMenu = ({ icon, label, children }: NavBarSubMenuProps) => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const insideRef = useDetectClickOutside({
+		onTriggered: () => setIsOpen(false),
+	})
+
+	return <NavBarMenuItemContainer ref={insideRef}>
+		<NavBarMenuItemLink icon={icon} label={label} showArrow={true} onClick={() => setIsOpen(!isOpen)}/>
+		<NavBarSubMenuContainer isOpen={isOpen}>
+			{children}
+		</NavBarSubMenuContainer>
+	</NavBarMenuItemContainer>
+}
+
+export interface NavBarMenuProps {
+	readonly children: DeepReadonly<ReactNode>
+}
+
+export const NavBarMenu = ({ children }: NavBarMenuProps) => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const insideRef = useDetectClickOutside({
+		onTriggered: () => setIsOpen(false),
+	})
+
+	return <NavBarRightBase ref={insideRef}>
+		<Hamburger onClick={() => setIsOpen(!isOpen)}><img src="data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAyMCAxNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNMjAgMlYwSDBWMkgyMFpNMjAgNlY4SDBWNkgyMFpNMjAgMTJWMTRIMFYxMkgyMFoiIGZpbGw9IndoaXRlIi8+DQo8L3N2Zz4="/></Hamburger>
+		<NavBarMenuContainer isOpen={isOpen}>
+			{children}
+		</NavBarMenuContainer>
+	</NavBarRightBase>
+}
 
 export default NavBar
